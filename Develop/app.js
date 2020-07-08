3f3d8390079dd7519883a1a38f2ad6f1589ac562
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-07 19:11:16 -0700

    Inquirer foundation set

--- Develop/app.js
+++ Develop/app.js
@@ -4,11 +4,175 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const teamMembersArr = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+function mainMenu() {
+    inquirer.prompt({
+        type: "list",
+        name: "choice",
+        message: "Hello! What would you like to do today?",
+        choices: [
+            "Add a team member",
+            "Exit and generate the HTML.",
+        ]
+    }).then(function ({ choice }) {
+        switch (choice) {
+            case "Add a team member":
+                console.log("Testing")
+                teamMemberPosition();
+                break;
+
+            case "Exit and generate the HMTL.":
+                console.log("Thank you!")
+        }
+    })
+
+
+    // 
+    // 
+    // 
+    // 
+    // Second Option -> Quit and Generate HTML
+    // 
+    // 
+    // 
+
+}
+
+mainMenu()
+
+function teamMemberPosition() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Team Member Position",
+            name: "position",
+            choices: [
+                "Intern",
+                "Engineer",
+                "Manager",
+            ]
+        }
+    ]).then(function (answers) {
+        if (answers.position === "Intern") {
+            console.log("Intern")
+            teamMemberInfo()
+        } else if (answers.position === "Engineer") {
+            console.log("Engineer")
+            teamMemberInfo()
+        } else if (answers.position === "Manager") {
+            console.log("Engineer")
+            teamMemberInfo()
+        } else {
+            console.log("Error")
+        }
+
+    })
+}
+
+function teamMemberInfo(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Team Member Name?",
+            name: "name"
+        },{
+            type: "input",
+            message: "Team Member ID?",
+            name: "id"
+        },{
+        type: "input",
+        message: "Team Member Email?",
+        name: "email"
+    }
+    ]).then(function (answers) {
+        console.log(answers)
+    })
+}
+
+// function teamMemberID(){
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "Team Member ID?",
+//             name: "id"
+//         }
+//     ]).then(function (answers) {
+//         console.log("id")
+//     })
+// }
+// function teamMemberEmail(){}
+
+// ]) .then(function (answers) {
+
+
+
+//     ,
+// {
+//     type: "input",
+//         message: "Team Member Name",
+//             name: "name"
+// }, {
+//     type: "number",
+//         message: "Team Member ID",
+//             name: "id"
+// }, {
+//     type: "input",
+//         message: "Team Member Email:",
+//             name: "email"
+// }
+
+// ]) .then(function (answers) {
+// console.log(answers)
+// answers.name.push(teamMembersArr)
+// console.log(teamMembersArr)
+// mainMenu()
+// Push answers into an array
+// 
+// Cycle back to menu
+// Add edit function?
+// Add new members from there
+// IF statements based off the position
+// var newpersonName = answers.name
+// var newpersonID = answers.id
+// var newpersonEmail = answers.email
+// var newpersonPosition = answers.position
+// const newPerson = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+
+
+
+
+
+    // let selectedTeamMember;
+    // for (let i = 0; i < teamMembersArr.length; i++) {
+//         if (teamMembersArr[i].name === answers.name) {
+//             selectedTeamMember = teamMembersArr[i]
+//         }
+//     }
+
+//     selectedTrainer.addPokemon(answers.pokemonName, answers.pokemonHp, answers.pokemonAtk);
+//     console.log(`${selectedTrainer.name} caught a ${answers.pokemonName}! Gotta catch 'em all!`)
+//     playGame();
+// })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
